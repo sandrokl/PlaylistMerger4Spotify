5cacc779523f7019427f385b2b37f109d5944767
Sandro Klostermann <sandro.kl.80@gmail.com>
2022-02-05 16:40:05 -0500

    Export playlists multi-platform now.

--- lib/helpers/import_export_helper.dart
+++ lib/helpers/import_export_helper.dart
@@ -5,45 +5,55 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:playlistmerger4spotify/database/database.dart';
 import 'package:playlistmerger4spotify/generated/l10n.dart';
 import 'package:playlistmerger4spotify/models/backup_info.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 Future<void> exportMergingDefinitions(BuildContext context) async {
-  String? result = await FilePicker.platform.getDirectoryPath(dialogTitle: "Choose folder to export file");
-  if (result != null) {
-    final info = await PackageInfo.fromPlatform();
+  final info = await PackageInfo.fromPlatform();
+  var backupInfo =
+      BackupInfo(appId: info.packageName, version: info.version, date: DateTime.now().toUtc().toIso8601String());
+  backupInfo.rules = [];
 
-    var backupInfo =
-        BackupInfo(appId: info.packageName, version: info.version, date: DateTime.now().toUtc().toIso8601String());
-    backupInfo.rules = [];
+  final db = Provider.of<AppDatabase>(context, listen: false);
+  var mergings = await db.playlistsDao.getMergedPlaylists();
 
-    final db = Provider.of<AppDatabase>(context, listen: false);
-    var mergings = await db.playlistsDao.getMergedPlaylists();
+  for (var destination in mergings) {
+    var r = Rule(destinationId: destination.playlistId, destinationName: destination.name);
+    r.sources = [];
 
-    for (var destination in mergings) {
-      var r = Rule(destinationId: destination.playlistId, destinationName: destination.name);
-      r.sources = [];
-
-      var sourcesId = await db.playlistsToMergeDao.getPlaylistsToMergeByDestinationId(destination.playlistId);
-      var sources = await db.playlistsDao.getPlaylistsByIdList(sourcesId.map((e) => e.sourcePlaylistId).toList());
-      for (var source in sources) {
-        r.sources!.add(Source(id: source.playlistId, name: source.name, owner: source.ownerId));
-      }
-
-      backupInfo.rules!.add(r);
+    var sourcesId = await db.playlistsToMergeDao.getPlaylistsToMergeByDestinationId(destination.playlistId);
+    var sources = await db.playlistsDao.getPlaylistsByIdList(sourcesId.map((e) => e.sourcePlaylistId).toList());
+    for (var source in sources) {
+      r.sources!.add(Source(id: source.playlistId, name: source.name, owner: source.ownerId));
     }
 
-    File file = File(join(result, "pm4s-export-${(DateTime.now().millisecondsSinceEpoch / 1000).floor()}.json"));
-    var jsonEncoder = const JsonEncoder.withIndent('  ');
-    file.writeAsString(jsonEncoder.convert(backupInfo.toJson()));
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(S.of(context).mergingDefinitionsSavedSuccessfully),
-      ),
-    );
+    backupInfo.rules!.add(r);
   }
+
+  final fileName = "pm4s-export-${(DateTime.now().millisecondsSinceEpoch / 1000).floor()}.json";
+  final directory = await getTemporaryDirectory();
+  final destinationFile = join(directory.path, fileName);
+
+  final file = File(destinationFile);
+  const jsonEncoder = JsonEncoder.withIndent("  ");
+  final stringContent = jsonEncoder.convert(backupInfo);
+  await file.writeAsString(stringContent);
+
+  await Share.shareFiles(
+    [file.path],
+    mimeTypes: ["application/json"],
+    subject: "teste1",
+    text: "teste2",
+  );
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(S.of(context).mergingDefinitionsSavedSuccessfully),
+    ),
+  );
 }
 
 Future<void> importMergingDefinitions(BuildContext context) async {
