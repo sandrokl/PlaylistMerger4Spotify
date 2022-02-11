92bc86aeb783ad941c423df845395bbc02656669
sandrokl <sandro.kl.80@gmail.com>
2022-02-11 10:47:34 -0500

    update example "get tracks"

--- lib/database/models/base/track.dart
+++ lib/database/models/base/track.dart
@@ -93,149 +93,91 @@ class Track extends DataClass implements Insertable<Track> {
           other.durationMs == durationMs);
 }
 
-
-
-// curl -X "GET" "https://api.spotify.com/v1/playlists/<playlist_id>/tracks?fields=items(track(id%2Cname%2Curi%2Cduration_ms%2Cartists(name)))&limit=10&offset=5"
-// {
-//   "items": [
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Haley Reinhart"
-//           }
-//         ],
-//         "duration_ms": 173500,
-//         "id": "0Rm2G83ELwkuEgpHlJcBPn",
-//         "name": "Can't Help Falling in Love",
-//         "uri": "spotify:track:0Rm2G83ELwkuEgpHlJcBPn"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Olivia O'Brien"
-//           }
-//         ],
-//         "duration_ms": 189000,
-//         "id": "0jllH0usRFD4LJkJnGK9Lf",
-//         "name": "Complicated",
-//         "uri": "spotify:track:0jllH0usRFD4LJkJnGK9Lf"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Alex Goot"
-//           },
-//           {
-//             "name": "Kurt Hugo Schneider"
-//           },
-//           {
-//             "name": "Megan Nicole"
-//           }
-//         ],
-//         "duration_ms": 146626,
-//         "id": "4Ejcw0qIiDjuaMtB5WDiBD",
-//         "name": "Stand By You",
-//         "uri": "spotify:track:4Ejcw0qIiDjuaMtB5WDiBD"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Jasmine Thompson"
-//           }
-//         ],
-//         "duration_ms": 172149,
-//         "id": "5fa6CrQwHwthEoCvTtd5Ig",
-//         "name": "Let Her Go",
-//         "uri": "spotify:track:5fa6CrQwHwthEoCvTtd5Ig"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "The Mayries"
-//           }
-//         ],
-//         "duration_ms": 183472,
-//         "id": "5l7gQEyycQWk10yVfXspVK",
-//         "name": "Don't Wanna Know - Acoustic Version",
-//         "uri": "spotify:track:5l7gQEyycQWk10yVfXspVK"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Ed Sheeran"
-//           }
-//         ],
-//         "duration_ms": 198777,
-//         "id": "73TXMz1i41sGfOuDg8gH4L",
-//         "name": "Candle In The Wind - 2018 Version",
-//         "uri": "spotify:track:73TXMz1i41sGfOuDg8gH4L"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Cat Power"
-//           }
-//         ],
-//         "duration_ms": 260760,
-//         "id": "7Kx832x6sRNGRy8o40u7Mr",
-//         "name": "Bad Religion",
-//         "uri": "spotify:track:7Kx832x6sRNGRy8o40u7Mr"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Hearts & Colors"
-//           }
-//         ],
-//         "duration_ms": 211000,
-//         "id": "3PEb2ARloVhBrffWcS8hyy",
-//         "name": "Lego House",
-//         "uri": "spotify:track:3PEb2ARloVhBrffWcS8hyy"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Boyce Avenue"
-//           },
-//           {
-//             "name": "Bea Miller"
-//           }
-//         ],
-//         "duration_ms": 222146,
-//         "id": "2PVU8zHILp6jK4Q7W6rkz8",
-//         "name": "We Can't Stop (feat. Bea Miller)",
-//         "uri": "spotify:track:2PVU8zHILp6jK4Q7W6rkz8"
-//       }
-//     },
-//     {
-//       "track": {
-//         "artists": [
-//           {
-//             "name": "Kina Grannis"
-//           }
-//         ],
-//         "duration_ms": 201933,
-//         "id": "7uuEfUMuPeQ7RlSWa0cES2",
-//         "name": "Can’t Help Falling in Love",
-//         "uri": "spotify:track:7uuEfUMuPeQ7RlSWa0cES2"
-//       }
-//     }
-//   ]
-// }
+/*
+https://api.spotify.com/v1/playlists/37i9dQZF1DXarRysLJmuju/tracks?fields=items(added_at%2Ctrack(id%2Cname%2Curi%2Cduration_ms%2Cartists(name)))%2Cprevious%2Cnext&limit=5&offset=5
+{
+  "items": [
+    {
+      "added_at": "2022-02-11T06:50:15Z",
+      "track": {
+        "artists": [
+          {
+            "name": "Michael Bublé"
+          }
+        ],
+        "duration_ms": 218390,
+        "id": "3tKggonCKJgCG7nroEH2np",
+        "name": "I'll Never Not Love You",
+        "uri": "spotify:track:3tKggonCKJgCG7nroEH2np"
+      }
+    },
+    {
+      "added_at": "2022-02-11T06:50:15Z",
+      "track": {
+        "artists": [
+          {
+            "name": "Elton John"
+          },
+          {
+            "name": "Dua Lipa"
+          },
+          {
+            "name": "PNAU"
+          }
+        ],
+        "duration_ms": 202735,
+        "id": "7rglLriMNBPAyuJOMGwi39",
+        "name": "Cold Heart - PNAU Remix",
+        "uri": "spotify:track:7rglLriMNBPAyuJOMGwi39"
+      }
+    },
+    {
+      "added_at": "2022-02-11T06:50:15Z",
+      "track": {
+        "artists": [
+          {
+            "name": "Tiësto"
+          },
+          {
+            "name": "Ava Max"
+          }
+        ],
+        "duration_ms": 164818,
+        "id": "18asYwWugKjjsihZ0YvRxO",
+        "name": "The Motto",
+        "uri": "spotify:track:18asYwWugKjjsihZ0YvRxO"
+      }
+    },
+    {
+      "added_at": "2022-02-11T06:50:15Z",
+      "track": {
+        "artists": [
+          {
+            "name": "Tyler Shaw"
+          }
+        ],
+        "duration_ms": 192560,
+        "id": "6OFXDyfd3QB3DhXGfSBZUY",
+        "name": "I See You",
+        "uri": "spotify:track:6OFXDyfd3QB3DhXGfSBZUY"
+      }
+    },
+    {
+      "added_at": "2022-02-11T06:50:15Z",
+      "track": {
+        "artists": [
+          {
+            "name": "Em Beihold"
+          }
+        ],
+        "duration_ms": 169237,
+        "id": "3o9kpgkIcffx0iSwxhuNI2",
+        "name": "Numb Little Bug",
+        "uri": "spotify:track:3o9kpgkIcffx0iSwxhuNI2"
+      }
+    }
+  ],
+  "next": "https://api.spotify.com/v1/playlists/37i9dQZF1DXarRysLJmuju/tracks?offset=10&limit=5&fields=items(added_at,track(id,name,uri,duration_ms,artists(name))),previous,next&locale=fr-CA,fr;q=0.9,en-CA;q=0.8,en;q=0.7,pt-BR;q=0.6,pt;q=0.5,en-US;q=0.4",
+  "previous": "https://api.spotify.com/v1/playlists/37i9dQZF1DXarRysLJmuju/tracks?offset=0&limit=5&fields=items(added_at,track(id,name,uri,duration_ms,artists(name))),previous,next&locale=fr-CA,fr;q=0.9,en-CA;q=0.8,en;q=0.7,pt-BR;q=0.6,pt;q=0.5,en-US;q=0.4"
+}
+*/
